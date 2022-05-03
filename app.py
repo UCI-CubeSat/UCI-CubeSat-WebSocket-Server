6694cea9789d4902cee1e9730183713d70cc4b6a
Chilly712 <chillinthestar@outlook.com>
2022-05-02 18:38:59 -0700

    Empty Commit to Trigger Heroku Deploy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from src.python.route.webSocketRoute import routes
 from src.python.config.appConfig import app, wsApp
 
-_ = [app.register_blueprint(route) for route in [routes]]
+[app.register_blueprint(route) for route in [routes]]
 
 
 if __name__ == '__main__':
