761baa26419a5b8b8969c1ebfb88963284de1d10
Chilly712 <chillinthestar@outlook.com>
2022-05-02 20:50:19 -0700

    Remove flask env error

--- src/python/config/appConfig.py
+++ src/python/config/appConfig.py
@@ -3,12 +3,8 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
 
-# flask config setting
-flaskHost: str | None = os.getenv("FLASK_HOST") if os.getenv("FLASK_HOST") else None
-flaskPort: str | None = os.getenv("FLASK_PORT") if os.getenv("FLASK_PORT") else None
-flaskEnv: str | None = os.getenv("FLASK_ENV") if os.getenv("FLASK_ENV") else "development"
-flaskDebug: bool = True if flaskEnv == "development" else False
 
+# flask config setting
 app: Flask = Flask(__name__)
 CORS(app)
 wsApp: SocketIO = SocketIO(app)
